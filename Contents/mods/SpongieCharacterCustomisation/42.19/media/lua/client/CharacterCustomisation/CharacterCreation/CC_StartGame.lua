08b90aee9c72d431f8b73f3218ab36a8748dd2bc
Spongie <lazyspongie@gmail.com>
2026-08-25 13:45:40 +0100

    commented out CC_StartGame

--- Contents/mods/SpongieCharacterCustomisation/42.19/media/lua/client/CharacterCustomisation/CharacterCreation/CC_StartGame.lua
+++ Contents/mods/SpongieCharacterCustomisation/42.19/media/lua/client/CharacterCustomisation/CharacterCreation/CC_StartGame.lua
@@ -1,15 +1,15 @@
----@diagnostic disable: duplicate-set-field
-require("CharacterCustomisation/CharacterCreation/CC_UpdateCharacterPreview")
+-- ---@diagnostic disable: duplicate-set-field
+-- require("CharacterCustomisation/CharacterCreation/CC_UpdateCharacterPreview")
 
 
--- removing customisation items on game start to see if it fixes the crashing
--- customisation items are readded after the player loads
-local oldOnOptionMouseDown = CharacterCreationMain.onOptionMouseDown
-function CharacterCreationMain:onOptionMouseDown(button, x, y)
+-- -- removing customisation items on game start to see if it fixes the crashing
+-- -- customisation items are readded after the player loads
+-- local oldOnOptionMouseDown = CharacterCreationMain.onOptionMouseDown
+-- function CharacterCreationMain:onOptionMouseDown(button, x, y)
 	
-	if button.internal == "NEXT" then
-		self:spn_remove_items()
-	end
+-- 	if button.internal == "NEXT" then
+-- 		self:spn_remove_items()
+-- 	end
 
-	oldOnOptionMouseDown(self, button, x, y)
-end
+-- 	oldOnOptionMouseDown(self, button, x, y)
+-- end
